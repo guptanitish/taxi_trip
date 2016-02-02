fca65ff46462cbd3412cbe1989ce78a1d77b3aea
Nitish <gupta.nitish@gmail.com>
2016-02-01 20:48:43 -0500

    clean up script

--- taxi_trips_nyc.py
+++ taxi_trips_nyc.py
@@ -1,33 +1,24 @@
 
-# coding: utf-8
-
-# In[25]:
-
 #this is potentially very interesting dataset. It has taxi trips in NYC for around a year. Let see it!
+#does some exploratory analysis of the data; outputs two graphs in pwd
 
+#get the input
 taxi_data_path = "/home/nitish/fellowship/taxi_data/trip_data_12.csv"
-
+import numpy as np
 import pandas as pd
 taxi_df = pd.read_csv(taxi_data_path)
-
-
-# In[26]:
 
 #ahh, the columns names have leading spaces, not nice.
 cols =taxi_df.columns.values
 for i in range(0,len(cols)):
     cols[i] = cols[i].strip()
 
-
-
 # In[27]:
 
 taxi_df.columns = cols
 
-
-# In[29]:
-
-class timestamp():  
+#need this class since the for parsing timestamp.
+class timestamp():
     def __init__(self,date):
         self.day = date.split(' ')[0]
         self.time = date.split(' ')[1]
@@ -36,15 +27,14 @@ class timestamp():
         self.dd = int(self.day.split('-')[2])
 
 
-# In[30]:
-
 import sys
 hour=[]
 day = []
+print 'Procesing the input'
 l = len(taxi_df)
 for i in range(0,l):
     if i%300000 ==0:
-        sys.stdout.write(str(i))
+        sys.stdout.write('.')
         sys.stdout.flush()
     t = timestamp(taxi_df.dropoff_datetime[i])
     hh = t.hh
@@ -52,40 +42,27 @@ for i in range(0,l):
     hour.append(hh)
     day.append(dd)
 
-
-# In[31]:
-
 taxi_df['hh']=hour
 taxi_df['dd']=day
 
 
-# In[34]:
+#ok, so i can predict the taxi demand
+#include can do this according to location, or time of day, etc. +
+#where you get most fares/tips
+#or even fare/time analysis
+# or on 4th of july
+#to prove that this is viable, we look at cabs around times sq last 10
+#days of Decmeber
 
-new_df = taxi_df[(taxi_df.dropoff_longitude<= -73.0) & (taxi_df.dropoff_longitude>= -74.0) &(taxi_df.dd >=22) &(taxi_df.dd <29)] 
-
-
-# In[81]:
+new_df = taxi_df[(taxi_df.dropoff_longitude<= -73.0) & (taxi_df.dropoff_longitude>= -74.0) &(taxi_df.dd >=22) &(taxi_df.dd <29)]
 
 relevant_df = new_df[['dd','hh','trip_time_in_secs']]
-
-
-# In[82]:
-
 grp1 = relevant_df.groupby('dd').count()
-
 
 # In[99]:
 
 grp2 =  relevant_df.groupby(['dd','hh']).count()
-
-
-# In[100]:
-
 grp3 =  relevant_df.groupby(['hh']).mean()
-
-
-# In[134]:
-
 counts = np.zeros(168)
 i = 0
 for val in grp2.trip_time_in_secs:
@@ -94,13 +71,11 @@ for val in grp2.trip_time_in_secs:
 
 counts = counts.reshape(24,7)
 
-np.argmax(counts,axis=0)       
+np.argmax(counts,axis=0)
 
 
 # In[189]:
 
-get_ipython().magic(u'matplotlib inline')
-import seaborn as sns
 import matplotlib.pyplot as plt
 christmas, = plt.plot(counts[:5,3],label='25 Dec')
 monday,=plt.plot(counts[:5,6],label='28 Dec')
@@ -109,28 +84,17 @@ plt.xlabel('Time of day (midnight to 5 am)')
 plt.title('Cab arriving near times square on Christmas day vs other day at night')
 plt.legend(handles=[christmas,monday])
 
-plt.show()
 plt.savefig('number_cabs.png')
-    
-
-
-# In[154]:
-
-print (counts[0:5:,2])
-print (counts[0:5:,0])
 
 
 # In[176]:
 
-get_ipython().magic(u'matplotlib inline')
-import seaborn as sns
 import matplotlib.pyplot as plt
 plt.plot(grp3.trip_time_in_secs)
 plt.ylabel('Mean trip distances ')
 plt.xlabel('Time of day, starting from midnight')
 plt.title('Mean trip distances during various times of day')
-plt.show()
-plt.savefig('trip_dist.png')    
+plt.savefig('trip_dist.png')
 
 
 # In[ ]:
